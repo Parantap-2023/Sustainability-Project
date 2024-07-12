4226218e9b1db44c1db47e4952be2fc69898060e
Parantap-2023 <parantapmishra2023@gmail.com>
2024-07-12 21:38:01 +0530

    Update LCA.py

--- LCA.py
+++ LCA.py
@@ -3,9 +3,9 @@ import pandas as pd
 import plotly.express as px
 from streamlit_option_menu import option_menu
 st.set_page_config(layout='wide',page_title='LCA',page_icon='bar_chart')
-col50,col51=st.columns([1,2])
+col50,col51=st.columns([1,18])
 with col50:
-    st.image('LCA SYMBOL IMAGE.png')
+    st.image('LCA SYMBOL IMAGE.png',width=80)
 with col51:    
     st.title('LIFE CYCLE ANALYSIS')
 glass=pd.read_excel('Data/GLASS.xlsx')
